d6bc2c51f6f8dea4a1ff48d75f115bc2e4e76f06
awfulbarbie <119601129+awfulbarbie@users.noreply.github.com>
2022-12-02 05:08:26 +0300

    Add files via upload

---
+++ lab1.2/lab1.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            while (1 != 0)
+            {
+                Console.WriteLine("Введите стороны треугольника");
+
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    b = Convert.ToDouble(Console.ReadLine());
+                    c = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка! Введен недопустимый символ.");
+                    continue;
+                }
+            }
+
+            double hypotenuse = Math.Pow(a, 2) + Math.Pow(b, 2);
+            if(hypotenuse == (Math.Pow(c, 2)))
+            {
+                Console.WriteLine("Треугольник являтся прямоугольным");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не является прямоугольным");
+            }
+        }
+    }
+}
---
+++ lab1yp/lab1yp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1yp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Введите сторону треугольника");
+            try
+            {
+                a = Convert.ToDouble(Console.ReadLine());
+                if (a <= 0)
+                {
+                    Console.WriteLine("Сторона треугольника не может иметь отрицательное или нулевое значение!");
+                }
+                else
+                {
+                    Console.Write("Радиус описанной окружности равен: ");
+                    Console.WriteLine(Math.Round(a / Math.Sqrt(3),3));
+                }
+            }
+            catch
+                { Console.WriteLine("Ошибка. Данные введены некорректно!"); }
+           
+        }
+    }
+}
